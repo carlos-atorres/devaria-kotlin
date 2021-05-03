201a69c58fed3b5e326b2c404c815ce4f57704b7
carlos-atorres <carlosalbertorres318@gmail.com>
2021-05-03 19:18:31 -0300

    Inputs, variáveis e constantes

---
+++ OlaMundo/src/InputVariaveisConstantes.kt
@@ -0,0 +1,21 @@
+fun main(argumentos : Array<String>) {
+
+    if(argumentos.isEmpty()) {
+        println("Favor informar um argumento válido")
+        return
+    }
+
+    val numero = argumentos[0].toIntOrNull() //val constante
+
+    if(numero == null){
+        println("O valor informado não é um número válido!")
+        return
+    }
+    println("o número digitado foi: $numero")
+
+    var soma = 0
+    soma = numero + numero
+    println("A soma do número + número é de: $soma")
+
+
+}
